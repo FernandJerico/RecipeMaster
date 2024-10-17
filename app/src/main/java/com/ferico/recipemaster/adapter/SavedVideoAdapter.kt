fab38da9ff4a25600daafdb9902f3e83fadf2f44
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-10-17 21:42:58 +0700

    add: item_saved_video.xml and SavedVideoAdapter.kt

---
+++ app/src/main/java/com/ferico/recipemaster/adapter/SavedVideoAdapter.kt
@@ -0,0 +1,35 @@
+package com.ferico.recipemaster.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ferico.recipemaster.R
+import com.ferico.recipemaster.data.Recipe
+
+class SavedVideoAdapter(private val recipeList: List<Recipe>) : RecyclerView.Adapter<SavedVideoAdapter.RecipeViewHolder>() {
+
+    class RecipeViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val imageRecipe: ImageView = v.findViewById(R.id.iv_img_content_saved_video)
+        val textRecipe: TextView = v.findViewById(R.id.tv_title_saved_video)
+        val ratingRecipe: TextView = v.findViewById(R.id.tv_rating_saved_video)
+        val username: TextView = v.findViewById(R.id.tv_username_saved_video)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_saved_video, parent, false)
+        return RecipeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        val recipe = recipeList[position]
+        holder.textRecipe.text = recipe.title
+        holder.ratingRecipe.text = recipe.rating
+        holder.username.text = recipe.username
+        holder.imageRecipe.setImageResource(recipe.imageResId)
+    }
+
+    override fun getItemCount() = recipeList.size
+}
